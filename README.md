5ebc99ca0f0e9d5ab8b20264b50e79a37d1beed7
pezexd <pedro9031@gmail.com>
2022-08-24 12:24:22 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-qed31o
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-qed31o)
---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+const formated = ref('');
+
+const format = (value) => {
+  const toNumber = parseFloat(value.replace(',', ''));
+  const handle = new Intl.NumberFormat('es-MX').format(toNumber);
+  const toStr = handle.toString();
+  value === '' ? (formated.value = '0') : (formated.value = toStr);
+};
+</script>
+
+<template>
+  <div>
+    <input v-model="formated" type="text" @input="format(formated)" />
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
